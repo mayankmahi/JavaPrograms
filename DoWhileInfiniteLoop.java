be45b110aec0e40ff9fbd50f449cbe68915dbd70
mkmahi07 <mayank.bytesbrick.com>
2022-07-10 14:17:45 +0530

    Do While Loop Basic Practice

---
+++ DoWhileInfiniteLoop.java
@@ -0,0 +1,7 @@
+public class DoWhileInfiniteLoop {
+    public static void main(String args[]){
+        do{
+            System.out.println("Infinite Loop");
+        } while (true);
+    }
+}
---
+++ InfiniteWhile.java
@@ -0,0 +1,7 @@
+public class InfiniteWhile {
+    public static void main(String args[]){
+        while(true){
+            System.out.println("While Infinite Loop");
+        }
+    }
+}
